d07d32b49f29b8953bbd9195106ce53ab1ceb885
Jacob Motes <jmotes2583@gmail.com>
2021-06-30 10:07:34 -0500

    server side get routes created for all positions

--- server/routes/consensus-rankings.js
+++ server/routes/consensus-rankings.js
@@ -2,10 +2,9 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
+//Will need 5 different get routes depending on position
 
-/**
- * GET route template
- */
+//All players
 router.get('/', (req, res) => {
   // GET route code here
   console.log('in get c-r rankings get router');
@@ -26,7 +25,7 @@ router.get('/', (req, res) => {
 }
 });
 
-//create a router to get Quarterbacks only
+//Quarterbacks only
 router.get('/qb', (req, res) => {
   // GET route code here
   console.log('in qb rankings get router');
@@ -47,11 +46,67 @@ router.get('/qb', (req, res) => {
 }
 });
 
-/**
- * POST route template
- */
-router.post('/', (req, res) => {
-  // POST route code here
+//Running backs only
+router.get('/rb', (req, res) => {
+  // GET route code here
+  console.log('in rb rankings get router');
+  if(req.isAuthenticated()) {
+  // Define query text to get from server
+  const queryText = `SELECT * FROM "players"
+  WHERE "user_id" is NULL AND "position" = 'RB'
+  ORDER BY SUBSTRING("position_rank" FROM '([0-9]+)')::BIGINT ASC;`
+  // use pool to retrieve the data
+  pool.query(queryText).then(result => {
+    res.send(result.rows);
+  }).catch( err => {
+    console.log("Error in server side GET:", err);
+    res.sendStatus(500);
+  })
+} else {
+  res.sendStatus(403);
+}
+});
+
+//Wide receivers only
+router.get('/wr', (req, res) => {
+  // GET route code here
+  console.log('in qb rankings get router');
+  if(req.isAuthenticated()) {
+  // Define query text to get from server
+  const queryText = `SELECT * FROM "players"
+  WHERE "user_id" is NULL AND "position" = 'WR'
+  ORDER BY SUBSTRING("position_rank" FROM '([0-9]+)')::BIGINT ASC;`
+  // use pool to retrieve the data
+  pool.query(queryText).then(result => {
+    res.send(result.rows);
+  }).catch( err => {
+    console.log("Error in server side GET:", err);
+    res.sendStatus(500);
+  })
+} else {
+  res.sendStatus(403);
+}
+});
+
+//Tight ends only
+router.get('/te', (req, res) => {
+  // GET route code here
+  console.log('in qb rankings get router');
+  if(req.isAuthenticated()) {
+  // Define query text to get from server
+  const queryText = `SELECT * FROM "players"
+  WHERE "user_id" is NULL AND "position" = 'TE'
+  ORDER BY SUBSTRING("position_rank" FROM '([0-9]+)')::BIGINT ASC;`
+  // use pool to retrieve the data
+  pool.query(queryText).then(result => {
+    res.send(result.rows);
+  }).catch( err => {
+    console.log("Error in server side GET:", err);
+    res.sendStatus(500);
+  })
+} else {
+  res.sendStatus(403);
+}
 });
 
 module.exports = router;
