933cbb9393272171deaa95f222e54907d03ad84c
Jacob Motes <jmotes2583@gmail.com>
2021-07-13 19:16:32 -0500

    readme updated

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -10,7 +10,7 @@ function AboutPage() {
   return (
     <div className="container">
       <div>
-        <h2>Technologies used in creating this application include:</h2>
+        <h2 className="header-spacing">Technologies used in creating this application include:</h2>
 
         <ul className="center">
           <li>React.js</li>
@@ -21,33 +21,6 @@ function AboutPage() {
           <li>Express.js</li>
           <li>PostgreSQL</li>
         </ul>
-
-        <h2 className="header-spacing">Toughest Challenge:</h2>
-
-          <ul>
-            <li className="center">One of the toughest challenges that I overcame was allowing the user to update the positional
-            ranking instead of the overall ranking when organizing the table by position only.
-            </li>
-          </ul>
-
-        <h2 className="header-spacing">Next Challenge:</h2>
-          <ul>
-            <li className="center">I would like to add drag and drop technology to the rankings table to allow the user to drag and drop their player rankings.
-            </li>
-          </ul>
-
-        <h2 className="header-spacing">Shoutouts:</h2>
-
-          <ul>
-            <li className="center">Thank you to Prime Digital Academy for making this all possible. 
-            The program has been everything that I hoped it would be and more.
-            </li>
-            <li className="list-spacing">I would also like to give a huge thank you to my instructor, Dane. He is an excellent
-            teacher and really has a way with connecting with students and making sure that they are understanding the content. 
-            </li>
-            <li className="list-spacing">I would also like to give a huge thank you to my cohort. Everyone is always willing to help
-            each other out and they have made this a very enjoyable experience.</li>
-          </ul>
 
       </div>
     </div>
